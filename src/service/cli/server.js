6bf39128e3a92e4bb5b0509b0716143afd8fcb29
twelwet <akimovmax@mail.ru>
2020-03-18 19:29:08 +0300

    #17 добавил express.json

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -11,20 +11,11 @@ const {
 const homeRouter = require(`./../routes/home.js`);
 
 const app = express();
+
 app.use(`/`, homeRouter);
+app.use(express.json());
+
 app.set(`json spaces`, 2);
-
-
-// [ВОПРОС]
-// Не понял в задании #17: "3. Подключите middleware для обработки JSON."
-// Нагуглил что middleware вот так подключается:
-//
-// app.use(express.json());
-//
-// Но куда эту эту строчку добавить - не разобрался.
-// А также не понял что должно делать JSON-middleware.
-// Ведь в моем случае res.json(...) уже отдает данные в нужном формате.
-
 
 module.exports = {
   name: CommandsNames.SERVER,
