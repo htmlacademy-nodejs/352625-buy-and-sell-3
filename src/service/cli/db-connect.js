ecd929ff750bfb828b517de1489b0ebb17568f4a
twelwet <akimovmax@mail.ru>
2020-09-26 22:18:11 +0300

    Connect to db using sequelize-packet

--- src/service/cli/db-connect.js
+++ src/service/cli/db-connect.js
@@ -6,15 +6,17 @@ const {getLogger} = require(`./../logger.js`);
 
 const logger = getLogger();
 
-const {Pool} = require(`pg`);
+const Sequelize = require(`sequelize`);
 
-const pool = new Pool({
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+    }
+);
 
 
 module.exports = {
@@ -24,12 +26,8 @@ module.exports = {
       try {
         logger.info(`Start connection to database...`);
 
-        const client = await pool.connect();
+        await sequelize.authenticate();
         logger.info(`Database connection established.`);
-
-        client.release();
-        pool.end();
-        logger.info(`Connection pool closed.`);
 
       } catch (err) {
         logger.error(`Can't connect to database. ${err}`);
