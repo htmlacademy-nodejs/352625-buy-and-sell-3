44d51377a0f19d36c9b3f806d5e1aecf58291403
twelwet <akimovmax@mail.ru>
2020-09-26 22:08:59 +0300

    Db connection uses ENV VARs

--- src/service/cli/db-connect.js
+++ src/service/cli/db-connect.js
@@ -9,11 +9,11 @@ const logger = getLogger();
 const {Pool} = require(`pg`);
 
 const pool = new Pool({
-  host: `localhost`,
-  port: 5432,
-  user: `postgres`,
-  database: `buy_and_sell`,
-  password: `123456`
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 
